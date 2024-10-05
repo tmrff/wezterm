1764262297d5ecac16c148a8fc6089fbf152ed81
Thomas <thomasdroff@gmail.com>
2024-10-05 22:28:34 +1300

    Add toggle messiah background key bind

--- wezterm.lua
+++ wezterm.lua
@@ -13,4 +13,29 @@ config.window_frame = {
 	font_size = 11,
 }
 
+wezterm.on("toggle-background", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	if not overrides.background then
+		overrides.background = {
+			{
+				source = {
+					File = "/Users/thomasroff/.config/wezterm/backgrounds/hands.png",
+				},
+				opacity = 0.9,
+			},
+		}
+	else
+		overrides.background = nil
+	end
+	window:set_config_overrides(overrides)
+end)
+
+config.keys = {
+	{
+		key = "B",
+		mods = "CTRL",
+		action = wezterm.action.EmitEvent("toggle-background"),
+	},
+}
+
 return config
